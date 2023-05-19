2d1175ac51958f6f339de34cd716e36057843c6c
Elvis O <elvis.osujic@gmail.com>
2023-05-19 12:49:37 +0100

    First commit

---
+++ src/test/java/com/bigveezus/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigveezus.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
